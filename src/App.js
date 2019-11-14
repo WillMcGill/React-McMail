e887028dd67c0f7abfdcfa089d6fde2146aed7ea
Will McGill <mcgill.will@yahoo.com>
2019-11-14 13:14:56 -0500

    pulled API token

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import Login from './Login';
 
-function App() {
+class App extends React.Component {
+
+apiPull(){axios.get('http://127.0.0.1:8000/')
+    .then(response => {
+      const emailData = response.data;
+      console.log(emailData);
+    })}
 
 
-
+render () {    
   return (
-    <div>Howdy</div>
-  );
-}
+    <div className = "App">
+      <Login />
+    </div>
+)}}
+
 
 export default App;
